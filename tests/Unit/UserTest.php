cbd961a153145c1b21ff191f19eb9887c59400ae
Marcus Christiansen <christiansen.marcus@gmail.com>
2019-09-10 16:08:20 +0200

    Added User Tests

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+use App\Notifications\SignupActivate;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * check if a user was created in database
+     *
+     * @return void
+     */
+    public function testUserCreate()
+    {
+        $user = factory(User::class)->create();
+
+        $this->assertDatabaseHas('users', [
+            'name'              => $user->name,
+            'email'             => $user->email,
+            'active'            => 0,
+            'activation_token'  => $user->activation_token,
+            'deleted_at'        => NULL
+        ]);
+    }
+
+    /**
+     * check if email was sent after user was created in database
+     *
+     * @return void
+     */
+    public function testEmailSentAfterUserCreated()
+    {
+        Notification::fake();
+
+        // Assert that no notifications were sent...
+        Notification::assertNothingSent();
+
+        $user = factory(User::class)->create();
+
+        // Assert a notification was sent to the given users...
+        Notification::assertSentTo(
+            [$user], SignupActivate::class
+        );
+    }
+
+    /**
+     * check if users are soft deleted only
+     *
+     * @return void
+     */
+    public function testUserIsSoftDeleted()
+    {
+        $user = factory(User::class)->create();
+
+        $user->delete();
+
+        $this->assertSoftDeleted('users', $user->toArray());
+    }
+}
